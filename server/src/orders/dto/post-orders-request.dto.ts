14ef088881477fb1d5ab4ba72ad5c18044a30dcf
apopov <apopov@eoil.ru>
2023-05-29 13:31:25 +0300

    .

--- server/src/orders/dto/post-orders-request.dto.ts
+++ server/src/orders/dto/post-orders-request.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator"
+
 export class PostOrdersRequestDto {
+
+    @IsNotEmpty()
     fio: string
+
+    @IsNotEmpty()
     name: string
+
+    @IsOptional()
     description: string
+
+    @IsNotEmpty()
     phone: string
 }
